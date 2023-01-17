ad66b5a30bf01fabf393bcad6bd35331a9109055
Vedprakash Upraity <vedprakashupraity@gmail.com>
2023-01-17 17:23:48 +0530

    update branches field in jira

--- scripts/jira-ticket-update.sh
+++ scripts/jira-ticket-update.sh
@@ -8,6 +8,7 @@ set -x
 #   $ARTIFACT_TAG - github tag used for deployment (release-5488a22)
 #   $JIRA_REF_LIST - whitespace separated jira ticket ids
 #   $JIRA_API_TOKEN - Jira API Token
+#   $DEPLOYED_BRANCH - deployed branch
 
 JIRA_REF_LIST_ARRAY=($(echo $JIRA_REF_LIST))
 RELEASE_DATE=$(date +'%Y-%m-%d')
@@ -22,15 +23,28 @@ COMPONENT_ID=$(curl --request GET \
 
 for ref in "${JIRA_REF_LIST_ARRAY[@]}"; do
 
-EXISTING_COMPONENTS_JSON=$(curl -s \
+curl -s \
     --url "https://autorama.atlassian.net/rest/api/3/issue/${ref}" \
     --user "devops@vanarama.co.uk:${JIRA_API_TOKEN}" \
-    --header 'Accept: application/json' \
-        | jq -c '.fields.components')
+    --header 'Accept: application/json' > jira_issue_response.json
+
+EXISTING_COMPONENTS_JSON=$(cat jira_issue_response.json \
+    | jq -c '.fields.components')
+
+EXISTING_BRANCHES_JSON=$(cat jira_issue_response.json \
+    | jq -c '.fields.customfield_10147')
 
 # customfield_10133 - Release Date
 # customfield_10132 - Release Environment
 # customfield_10114 - Release Tag
+# customfield_10147 - Branches
+
+if [[ -z $DEPLOYED_BRANCH ]]; then
+    BRANCH_LIST_JSON=$(echo ${EXISTING_BRANCHES_JSON} | jq -c )
+else
+    BRANCH_LIST_JSON=$(echo ${EXISTING_BRANCHES_JSON} | jq -c ". |= . + [\"${DEPLOYED_BRANCH}\"]")
+fi
+
 jira_payload() {
 cat <<EOF
 {
@@ -53,6 +67,11 @@ cat <<EOF
         "components": [
             {
                 "set": $(echo ${EXISTING_COMPONENTS_JSON} | jq ". |= . + [{\"id\": \"${COMPONENT_ID}\"}]")
+            }
+        ],
+        "customfield_10147": [
+            {
+                "set": $(echo $BRANCH_LIST_JSON)
             }
         ]
     }
